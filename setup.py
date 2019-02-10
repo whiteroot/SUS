025f09f0b798e1f69dd246605262c4dfced82199
tux <tux@nunux>
2019-02-10 17:57:17 +0100

    fix python version issue with windows

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import subprocess
+import sys
 
-cmde = 'python3 -m pip install -r requirements.txt'
+if sys.version_info.major < 3:
+    print ('please use python3')
+    sys.exit(1)
+
+cmde = 'python -m pip install -r requirements.txt'
 p = subprocess.run(cmde.split())
